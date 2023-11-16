12b7bb3ef982d880032cdf5992cd612cae5ade81
natalianero <nataliasilva10@gmail.com>
2023-11-16 11:49:12 -0300

    Anatomia das Classes - 3

---
+++ src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+    public static void main (String [] args){
+        
+        String primeiroNome = "Natália";
+        String segundoNome = "Silva";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
